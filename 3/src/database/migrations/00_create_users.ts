281a75ad13a8df026061562a4eb2023107bf4a52
ricsonl <ricsonluizv@gmail.com>
2020-07-14 23:25:55 -0300

    comments

--- 3/src/database/migrations/00_create_users.ts
+++ 3/src/database/migrations/00_create_users.ts
@@ -1,12 +1,12 @@
 import Knex from 'knex';
 
 export async function up(knex:Knex){
-    return knex.schema.createTable('user', table => {
+    return knex.schema.createTable('user', table => { // cria a tabela 'user' com campos 'id', 'nome' e 'sobrenome'
         table.increments('id').primary();
         table.string('nome').notNullable();
         table.string('sobrenome').notNullable();
     });
 }
-export async function down(knex:Knex){
+export async function down(knex:Knex){ // desfaz a acao caso necessario
     return knex.schema.dropTable('user');
 }
--- 3/src/routes.ts
+++ 3/src/routes.ts
@@ -3,34 +3,43 @@ import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.post('/users', async (req, res) => {
+routes.post('/users', async (req, res) => { // CADASTRO DE USUARIO
+
     const _user = req.body;
     
-    const inserted = await knex('user').insert({
+    const inserted = await knex('user').insert({ // insere os usuario no banco e retorna uma lista com os id's cadastrados
         nome: _user.nome,
         sobrenome: _user.sobrenome,
     });
 
-    res.json({
+    if(inserted.length == 0){ // caso o usuario nao tenha sido inserido
+        res.status(500).json({ message: 'Erro: falha no cadastro do usuario' });
+    }
+
+    res.json({ // retorna o id do usuario inserido
         id: inserted[0]
     });
 });
 
-routes.get('/users', async (req, res) => {
+routes.get('/users', async (req, res) => { // LISTAGEM DE USUARIOS
+
     const _users = await knex('user').select('*');
+
     res.json(_users);
+
 });
 
-routes.get('/users/:id', async (req, res) => {
-    const _id = Number(req.params.id);
+routes.get('/users/:id', async (req, res) => { // BUSCA DE USUARIO POR ID
 
-    const _user = await knex('user').where('id', _id).first();
+    const _id = Number(req.params.id); // pega o id solicitado pela rota (por exemplo, localhost:3333/users/2 para buscar id "2")
 
-    if (!_user){
+    const _user = await knex('user').where('id', _id).first(); // busca no banco de dados por um usuario com id igual ao solicitado
+
+    if (!_user){ // caso a busca retorne nulo
         res.status(404).json({ message: 'Usuario nao encontrado'});
     }
     
-    res.json({
+    res.json({ // retorna nome e sobrenome do usuario
         nome: _user.nome,
         sobrenome: _user.sobrenome,
     });
